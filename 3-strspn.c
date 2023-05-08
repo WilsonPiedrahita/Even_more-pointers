d41e3c9a8ace860356f263a505057eb29699d491
root <root@DESKTOP-DHALUET>
2023-05-07 22:23:35 -0500

    archivo 3

---
+++ 3-strspn.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int count = 0; 
+int i, j;
+int found; 
+for (i = 0; s[i] != '\0'; i++)
+{
+    found = 0;
+    for (j = 0; accept[j] != '\0'; j++)
+    {
+        if (s[i] == accept[j])
+        {
+            count++;
+            found = 1;
+            break;
+        }
+
+    }
+    if (found == 0)
+    break;
+}
+return count;
+}
